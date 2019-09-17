ad4ed89d9125ae220521ab3aa85ed2f986d73ab3
Montaser Zaghmout <mont0004@edu.easj.dk>
2019-09-17 12:48:11 +0200

    Fjernede elementer fra forrige opgave

--- slideshow.js
+++ slideshow.js
@@ -2,27 +2,18 @@
     'use strict';
 
     // Tilføjer plugin til fn så det kan bruges normalt
-    $.fn.font = function (options) {
+    $.fn.slideshow = function (options) {
 
         // Options
         // Sætter default options, hvis der ikke sendes noget med
         const defaultOptions = {
-            font:{
-                txtsize: '100px'
-            }
+
         };
 
         const opts = $.extend(true, {}, defaultOptions, options);
 
         // Går gennem alle elementer der matcher selector = idx=index, el=element
         return this.each(function (idx, el) {
-            $(el).css({
-                'font-size': opts.font.txtsize
-            });
-
-            $(el).click(function () {
-                $(this).fadeOut(3000);
-            });
 
         });
     };
